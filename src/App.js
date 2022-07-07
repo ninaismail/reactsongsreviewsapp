46a1e15b4e6016ac8176cf23df55def570a91bdb
najat <najatt.ismail@gmail.com>
2022-07-07 11:18:10 +0300

    displayed songs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import axios from "axios";
-
 import {useState, useEffect} from 'react';
 function App(){
 // The endpoint is the value we get from the input 
@@ -8,6 +7,7 @@ const [endPoint, setEndPoints] = useState("")
 //  the container is the data we get from the api
 const [trackscontainer,setTracksContainer] = useState([])
 // whenever i type something this will call the fetchMe function
+
 useEffect(() => {
   fetchData()
 },[])
@@ -15,50 +15,65 @@ useEffect(() => {
   function fetchData() {
     const options = {
       method: 'GET',
-      url: 'https://genius-song-lyrics1.p.rapidapi.com/search',
-      params: {q: 'Alan Walker', per_page: '10', page: '1'},
+      url: 'https://genius.p.rapidapi.com/artists/16775/songs',
       headers: {
-        'X-RapidAPI-Key': '4b544470eemsha6cb3176c9530e2p1425c4jsn406add2a21e6',
-        'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+        'X-RapidAPI-Key': '5b5555a166msh1b6aac54714b238p16c4b8jsnd3a5d085a16d',
+        'X-RapidAPI-Host': 'genius.p.rapidapi.com'
       }
     };
-axios.request(options).then(function (response) {
-  console.log(response.data.response.hits)
-setTracksContainer(response.data.response.hits)
+axios.request(options, {timeout: 500}).then(function (response) {
+  console.log(response.data.response.songs)
+setTracksContainer(response.data.response.songs)
 }).catch(function (error) {
 	console.error(error);
 });
-
 }
 
 const onChangeHandleInput = (e) => {
 setEndPoints(e.target.value)
  } 
+ const optionsendPoint = {
+  method: 'GET',
+  url: 'https://genius.p.rapidapi.com/search',
+params: {q:  {endPoint}},
+  headers: {
+    'X-RapidAPI-Key': '5b5555a166msh1b6aac54714b238p16c4b8jsnd3a5d085a16d',
+    'X-RapidAPI-Host': 'genius.p.rapidapi.com'
+  }
+};
 // to prevent page refreshing every time we click search
 const submitHandler = (e) => {
   e.preventDefault()
+axios.request(optionsendPoint, {timeout: 500}).then(function (response) {
+  console.log(response.data.response.hits)
+setTracksContainer(response.data.response.hits)
+}).catch(function (error) {
+	console.error(error);
+});
 }
 
   return (
 <div className="App">
-<form className="input-group" onSubmit={submitHandler}>
+<form className="input-group" >
   <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" 
   value={endPoint}
   onChange={onChangeHandleInput}/>
-  <input type="submit" className="btn btn-outline-primary" placeholder="Search" />
+  <input type="submit" className="btn btn-outline-primary" placeholder="Search" onClick={submitHandler}/>
 </form>
 <div class="card-group">
   <div class="row">
 {trackscontainer.map((item) => 
+
+
 (<div class="col=xl-2 col-lg-3 col-md-6 col-sm-12">
 <div class="card" key={item.id}>
-<img src={item.result.song_art_image_thumbnail_url}/>
+<img src={item.song_art_image_thumbnail_url}/>
     <div class="card-body">
-      <h5 class="card-title">{item.result.title}</h5>
-      <p class="card-text">{item.result.artist_names}</p>
+      <h5 class="card-title">{item.full_title}</h5>
+      <p class="card-text">{item.artist_names}</p>
     </div>
     <div class="card-footer">
-    <a href={item.result.url}>Check out the lyrics on Genius</a>
+    <a href={item.url}>Check out the lyrics on Genius</a>
     </div>
   </div>
   </div>
